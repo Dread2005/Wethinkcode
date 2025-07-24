ca8415372b5a5efa98e91b265e9516dc068c2da6
Dread2005 <tshifhiwachedzafordjr@gmail.com>
2025-07-24 12:32:44 +0200

    question 1 of week 1 completed

---
+++ code/week_1/deep_thought.py
@@ -0,0 +1,9 @@
+forty2 = input('What is the Answer to the Great Question of Life, the Universe, and Everything?: ').lower()
+if forty2 == '42':
+    print('yes')
+elif forty2 == 'forty two':
+    print('yes')
+elif forty2 == 'forty-two':
+    print('yes')
+else:
+    print('No')
