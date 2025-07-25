61cc7e16ed1750847cd8c9a59905b5202e6996fb
Dread2005 <tshifhiwachedzafordjr@gmail.com>
2025-07-25 17:57:54 +0200

    Changes to the am, pm clock time in the convert function, check 'if and else' statments

--- code/week_1/meal.py
+++ code/week_1/meal.py
@@ -1,9 +1,9 @@
 def main():
-    time = input('What time is it? ').lower()
+    time = input('What time is it?  ').lower()
     time_lst = convert(time)
-    #print(type(time_lst[0]))
+    #print((time_lst[0]))
 
-    if type(time_lst[0]) == type(1):
+    if type(time_lst[0]) == type(1) and type(time_lst[1]) == type(1):
         if time_lst[0] <= 12:
             print('Breakfast')
         elif time_lst[0] > 12 and time_lst[0] < 18:
@@ -12,11 +12,11 @@ def main():
             print("Dinner")
 
     else:
-        if 'am' in time_lst and time_lst[0] < 12:
+        if time_lst[0] == 'am' and time_lst[1] <= 12:
             print('Breakfast')
-        elif 'pm' in time_lst and time_lst[0] >= 12 and time_lst[0] < 18:
+        elif time_lst[0] == 'pm' and time_lst[1] >= 1 and time_lst[1] < 6:
             print("Lunch")
-        else:
+        elif time_lst[0] == 'pm' and time_lst[1] >= 6:
             print('Dinner')
 
 
@@ -25,8 +25,12 @@ def convert(time):
         time_split = time.split(":")
         time_lst = [int(time_split[0]), int(time_split[1])]
     else:
-        time_split = [int(time[:-2]), time[2:]]
+        if len(time) == 4:
+            time_split = [time[2:], int(time[:-2])]
+        else:
+            time_split = [time[1:], int(time[:-2])]
         time_lst = time_split
+    print(time_lst)
     return time_lst
 
 if __name__ == '__main__':
