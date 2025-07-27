aab484c741f0676f2b5e87fc901b11a7c7e00b37
Dread2005 <tshifhiwachedzafordjr@gmail.com>
2025-07-27 12:56:46 +0200

    I completley changed the code, using functions (which wasnt necessary for me to fix the code) the most important part is in the snake_cas function, I simply changed the specific snake list index to have the '_' plus the value in lower case

--- code/week_2/camelcase.py
+++ code/week_2/camelcase.py
@@ -1,22 +1,22 @@
 
-camel_case_input = input('Enter here: ')
+# camel_case_input = input('Enter here: ')
 snake = []
 snake_ = []
 indx = None
-indxlst = []
-caps = []
-for n in camel_case_input:
-    snake.append(n)
-    if n.isupper():
-        caps.append(n)
-print(snake)
+# indxlst = []
+# caps = []
+# for n in camel_case_input:
+#     snake.append(n)
+#     if n.isupper():
+#         caps.append(n)
+# print(snake)
 
 # while len(indxlst) != len(caps):
-for item in snake:
-    if item.isupper():
-        snake.insert(int(snake.index(item)), '_')
+# for item in snake:
+#     if item.isupper():
+#         snake.insert(int(snake.index(item)), '_')
 
-print(snake)
+# print(snake)
 # for n in snake:
 #     if n.isupper():
 #         indx = snake.index(n)
@@ -26,3 +26,20 @@ print(snake)
 
 # snake_ = ' '.join(snake)
 # print(snake_)
+def camel_cas():
+    global snake
+    camel_case_input = input('Enter here: ')
+    for n in camel_case_input:
+        snake.append(n)
+
+def snake_cas():
+    global indx
+    global snake_
+    for n in snake:
+        if n.isupper():
+            snake[snake.index(n)] = '_'+n.lower()
+    snake_ = ' '.join(snake)
+
+camel_cas()
+snake_cas()
+print(snake_)
