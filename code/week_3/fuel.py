832aa84e2dca48a96fdb26d5823d2bccc2c6055b
Dread2005 <tshifhiwachedzafordjr@gmail.com>
2025-07-31 18:23:39 +0200

    Completly debugged and working perfectly

--- code/week_3/fuel.py
+++ code/week_3/fuel.py
@@ -2,25 +2,32 @@ def function():
     ans = None
     fraction = input('Fraction: ')
     frac_splt = fraction.split('/')
-
-    for i in frac_splt:
-        frac_splt[frac_splt.index(i)]  = int(i)
+    try:
+        for i in frac_splt:
+            frac_splt[frac_splt.index(i)]  = int(i)
         #print(frac_splt)
-    ans = (frac_splt[0]/frac_splt[1])*100
+        ans = int((frac_splt[0]/frac_splt[1])*100)
+        if ans < 0:
+            raise ValueError
+    except(ValueError, TypeError, ZeroDivisionError):
+        return function()
+
 
     if ans == 100:
         ans = 'Full'
     elif ans == 0:
-        ans = 'False'
+        ans = 'Empty'
 
     if type(ans) is str:
         return ans
-    elif type(ans) is int or type(ans) is float:
+    elif type(ans) is int:
         ans = f'%{ans}'
         return ans
-        
 
-try:
-    print(function())
-except(ValueError, TypeError, ZeroDivisionError):
-    print(function())
+func = function()
+print(func)
+
+# try:
+#     print(function())
+# except(ValueError, TypeError, ZeroDivisionError):
+#     print(function())
